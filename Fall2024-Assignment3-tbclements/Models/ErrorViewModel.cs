133fba001c74b3c9c73484ba7f3a253e28568f82
tclem404 <tclem404@gmail.com>
2024-10-07 13:42:33 -0500

    Started Project

---
+++ Fall2024-Assignment3-tbclements/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fall2024_Assignment3_tbclements.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
